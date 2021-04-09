f7dd253345a3821a85d63ace755bb3d9528fd477
Edvin <edvinpacheco03@gmail.com>
2021-04-08 20:22:50 -0600

    Se agregaron las rutas con cambios finales

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue';
 import Inicio from '../views/Inicio.vue';
+import Prueba from '../views/Prueba.vue';
 
 
 //PacientesAz
@@ -23,6 +24,7 @@ import Newdating from '../views/citas/New-dating.vue';
 import Allusers from '../views/usuarios/All-users.vue';
 import Editusers from '../views/usuarios/Edit-users.vue';
 import Newusers from '../views/usuarios/New-users.vue';
+
  
 
 Vue.use(VueRouter)
@@ -31,73 +33,125 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      libre: true
+    }
   },
   {
     path: '/inicio',
     name: 'Inicio',
-    component: Inicio
+    component: Inicio,
+    meta: {
+      loggeado: true
+    }
   },
   {
     path: '/pacientes/dashboard',
     name: 'Dashboard',
-    component: Dashboard
+    component: Dashboard,
+    meta: {
+      loggeado: true
+    }
   },
   {
     path: '/pacientes/editar/:id',
     name: 'Editar',
-    component: Editar
+    component: Editar,
+    meta: {
+      loggeado: true
+    }
   },
   {
     path: '/pacientes/nuevo',
     name: 'Nuevo',
-    component: Nuevo
+    component: Nuevo,
+    meta: {
+      loggeado: true
+    }
   },
   {
     path: '/notas/all-notes',
     name: 'All-notes',
-    component: Allnotes
+    component: Allnotes,
+    meta: {
+      loggeado: true
+    }
   },
   {
     path: '/notas/edit-notes/:id',
     name: 'Edit-notes',
-    component: Editnotes
+    component: Editnotes,
+    meta: {
+      loggeado: true
+    }
   },
   {
     path: '/notas/new-notes',
     name: 'New-notes',
-    component: Newnotes
+    component: Newnotes,
+    meta: {
+      loggeado: true
+    }
   },
   {
     path: '/citas/all-dating',
     name: 'All-dating',
-    component: Alldating
+    component: Alldating,
+    meta: {
+      loggeado: true
+    }
   },
   {
     path: '/citas/edit-dating/:id',
     name: 'Edit-dating',
-    component: Editdating
+    component: Editdating,
+    meta: {
+      loggeado: true
+    }
   },
   {
     path: '/citas/new-dating',
     name: 'New-dating',
-    component: Newdating
+    component: Newdating,
+    meta: {
+      loggeado: true
+    }
   },
   {
     path: '/usuarios/all-users',
     name: 'All-users',
-    component: Allusers
+    component: Allusers,
+    meta: {
+      loggeado: true
+    }
   },
   {
     path: '/usuarios/edit-users/:id',
     name: 'Edit-users',
-    component: Editusers
+    component: Editusers,
+    meta: {
+      loggeado: true
+    }
   },
   {
     path: '/usuarios/new-users',
     name: 'New-users',
-    component: Newusers
-  }
+    component: Newusers,
+    meta: {
+      loggeado: true
+    }
+  },
+  {
+    path: '/prueba',
+    name: 'prueba',
+    component: Prueba,
+    meta: {
+      loggeado: true
+    }
+    
+  },
+
 ]
 
 
@@ -105,6 +159,23 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+function existToken() {
+  return !!localStorage.getItem("token");
+}
+
+router.beforeEach((to, from, next) => {
+  if(to.matched.some(record => record.meta.libre)){
+    next();
+  } else if (existToken()){
+    //eslint-disable-next-line
+    if (to.matched.some(record => record.meta.loggeado)){
+      next();
+    }
+  } else {
+    next({path:'/'});
+  }
 })
 
 export default router
